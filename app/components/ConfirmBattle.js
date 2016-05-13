729af2b1f41109955c6170d1e4b5cc67a01919ef
Krešimir Čoko <kresimircoko@gmail.com>
2016-05-13 11:42:15 +0200

    Video 5

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,21 @@
 //stateless function
 var React = require('react')
 
+function puke(object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+};
+
 function ConfirmBattle(props) {
-  return (
-    <div> Confirm Battle </div>
-  )
+    if(props.isLoading) {
+      return (
+        <p> Loading </p>
+      )
+    }
+    else {
+      return (
+        <div> Confirm Battle: {puke(props)} </div>
+      )
+    }
 }
+
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle')
 
+var githubHelpers = require('../utils/githubHelpers')
+
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
@@ -13,9 +15,23 @@ var ConfirmBattleContainer = React.createClass({
     }
   },
 
+  componentDidMount: function() {
+    var query = this.props.location.query;
+    // Fetch info from github then update the state -- that info is called "promise"
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players [1]]
+        })
+      }.bind(this))
+  },
+
   render: function() {
     return (
-      <ConfirmBattle />
+      <ConfirmBattle
+        isLoading= {this.state.isLoading}
+        playersInfo={this.state.playersInfo} />
     )
   }
 });
